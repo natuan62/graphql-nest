a97bdc88bf04f31ff8ade4e522ce7bdc8671828a
Tuan Nguyen <tuan@rowboatsoftware.com>
2022-01-21 10:48:47 +0700

    add filters type

--- src/person/person.types.ts
+++ src/person/person.types.ts
@@ -1,8 +1,26 @@
 import { Field, InputType, InterfaceType } from '@nestjs/graphql';
-
+import { GraphQLJSONObject } from 'graphql-type-json';
 @InputType()
 @InterfaceType('BasePerson')
 export class CreatePersonInput {
   @Field()
   name: string;
+}
+
+@InputType()
+export class PersonFilters {
+  @Field(() => Number, { nullable: true })
+  skip?: number;
+
+  @Field(() => Number, { nullable: true })
+  limit?: number;
+
+  @Field(() => GraphQLJSONObject, { nullable: true })
+  fields?: string[];
+
+  @Field(() => GraphQLJSONObject, { nullable: true })
+  sort?: object;
+
+  @Field(() => GraphQLJSONObject, { nullable: true })
+  where?: object;
 }
